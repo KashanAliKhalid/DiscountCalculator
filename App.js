526a42cac12d482e39bbd0cbd0b1b5f9592c2ce8
Kashan Ali Khalid <kashanali_khalid@yahoo.com>
2020-11-28 19:32:13 +0500

    Design finalised

--- App.js
+++ App.js
@@ -14,101 +14,113 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput,
+    TouchableOpacity
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state={
+
+        }
+    }
+
+  render(): React$Node {
+    return(
+        <View style={styles.container}>
+          <Text style={styles.title}>DiscountAPP</Text>
+
+          <TextInput
+              style={styles.input}
+              placeholder="Original Price"
+          />
+
+          <TextInput
+              style={styles.input}
+              placeholder="Discount Percentage"
+          />
+
+          <TouchableOpacity style={styles.button}><Text style={styles.buttontext}>SAVE</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.button2}><Text style={styles.buttontext}>CALCULATE</Text></TouchableOpacity>
+
+        </View>
+        )
+
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container:{
+    backgroundColor:'#003f5c',
+    width:'100%',
+    height:'100%',
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+
+  input:
+      {
+        width:'85%',
+        height: '7%',
+        borderWidth:1,
+        borderRadius:50,
+        textAlign:'center',
+        borderColor:'#465881',
+        backgroundColor: '#465881',
+        color:'white',
+        fontSize:20,
+        marginTop:20,
+      },
+
+  title:
+      {
+        color:'#f05a5a',
+        fontSize: 50,
+        marginBottom:20,
+        fontWeight: 'bold',
+        fontFamily: 'notoserif'
+      },
+
+  button:
+      {
+        width:'85%',
+        height: '7%',
+        borderWidth:1,
+        borderRadius:50,
+        borderColor:'#f05a5a',
+        backgroundColor: '#f05a5a',
+        display:'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 40,
+      },
+
+  buttontext:
+{
+  color:'white',
+  fontSize:17,
+  textAlign:'center',
+  letterSpacing:8
+},
+
+  button2:
+      {
+        width:'85%',
+        height: '7%',
+        borderWidth:1,
+        borderRadius:50,
+        borderColor:'#f05a5a',
+        backgroundColor: 'transparent',
+        display:'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop:10,
+      },
+
 });
 
 export default App;
